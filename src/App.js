e1582aeb1b7c2b73f8754a8cbb34a05aac17c43d
Alex Martin <atonymartin20@yahoo.com>
2021-08-16 22:23:10 -0500

    App routing added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Features from './components/features';
+import Homepage from './components/homepage';
+import Pricing from './components/pricing';
+import Stories from './components/stories';
+
+class App extends React.Component {
+	render () {
+		return (
+			<div className='app-outside-container'>
+				<Switch>
+					<Route exact path='/'>
+						<Homepage />
+					</Route>
+					<Route exact path='/features'>
+						<Features />
+					</Route>
+					<Route exact path='/pricing'>
+						<Pricing />
+					</Route>
+					<Route exact path='/stories'>
+						<Stories />
+					</Route>
+					<Route>
+						<Homepage />
+					</Route>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
