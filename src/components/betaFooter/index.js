93394fd2705d511dcf57a0bb0294a3df26926af2
Alex Martin <atonymartin20@yahoo.com>
2021-08-19 22:26:12 -0500

    betaFooter styled for mobile

--- src/components/betaFooter/index.js
+++ src/components/betaFooter/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import RightArrow from '../../assets/shared/desktop/arrow.svg';
+import RightArrow from '../../assets/shared/desktop/white-arrow.svg';
+
+import '../../css/betaFooter.css';
 
 class BetaFooter extends React.Component {
     render () {
         return (
-            <div>
-                <h1>We're in Beta. Get your invite today!</h1>
-  
-                <NavLink to='/'>
-                    Get an invite
-                    <img src={RightArrow} alt='Right Arrow' />
-                </NavLink>
+            <div className='outside-beta-footer-container'>
+                <div className='beta-footer-gradient-div' />
+
+                <div className='inside-beta-footer-container'>
+                    <h1>We're in Beta. Get your invite today!</h1>
+    
+                    <NavLink to='/'>
+                        Get an invite
+                        <img src={RightArrow} alt='Right Arrow' />
+                    </NavLink>
+
+                </div>
             </div>
         )
     }
