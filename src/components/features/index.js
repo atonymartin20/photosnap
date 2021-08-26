dd84f25d5af733eb87b3ed6d7d9dbc0ec0b3d292
Alex Martin <atonymartin20@yahoo.com>
2021-08-25 22:09:11 -0500

    Features Component classes added for all breakpoints

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -13,26 +13,31 @@ import EmbedIMG from '../../assets/features/desktop/embed.svg';
 import NoLimitIMG from '../../assets/features/desktop/no-limit.svg';
 import ResponsiveIMG from '../../assets/features/desktop/responsive.svg';
 
+import '../../css/features.css';
+
 class Features extends React.Component {
     render () {
         return (
-            <div>
+            <div className='features-outside-container'>
                 <Navbar />
 
-                <div>
-                    <div>
-                        <div>
-                            <h1>Features</h1>
-                            <span>We make sure all of our features are designed to be loved by every aspiring and even 
-                            professional photograpers who wanted to share their stories.</span>
+                <div className='features-inside-container'>
+                    <div className='features-inside-top-container'>
+                        <div className='features-inside-top-text-div'>
+                            <div className='features-linear-gradient' />
+                            <div className='features-spacing-div'>
+                                <h1>Features</h1>
+                                <span>We make sure all of our features are designed to be loved by every aspiring and even 
+                                professional photograpers who wanted to share their stories.</span>
+                            </div>
                         </div>
-                        <img src={HeroIMGDesktop} alt='camera' />
-                        <img src={HeroIMGTablet} alt='camera' />
-                        <img src={HeroIMGMobile} alt='camera' />
+                        <img src={HeroIMGDesktop} alt='camera' className='hero-img-desktop' />
+                        <img src={HeroIMGTablet} alt='camera' className='hero-img-tablet' />
+                        <img src={HeroIMGMobile} alt='camera' className='hero-img-mobile' />
                     </div>
 
-                    <div>
-                        <div>
+                    <div className='features-content-container'>
+                        <div className='features-feature-card features-content-large-card'>
                             <img src={ResponsiveIMG} alt='Responsive icon' />
                             <h3>100% Responsive</h3>
                             <span>No matter which the device you’re on, our site is fully responsive and stories 
@@ -40,7 +45,7 @@ class Features extends React.Component {
                             </span>
                         </div>
 
-                        <div>
+                        <div className='features-feature-card features-content-small-card'>
                             <img src={NoLimitIMG} alt='Infinity sign' />
                             <h3>No Photo Upload Limit</h3>
                             <span>Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share 
@@ -48,7 +53,7 @@ class Features extends React.Component {
                             </span>
                         </div>
 
-                        <div>
+                        <div className='features-feature-card features-content-large-card'>
                             <img src={EmbedIMG} alt='Embed icon' />
                             <h3>Available to Embed</h3>
                             <span>Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google 
@@ -56,7 +61,7 @@ class Features extends React.Component {
                             </span>
                         </div>
 
-                        <div>
+                        <div className='features-feature-card features-content-large-card'>
                             <img src={CustomDomainIMG} alt='Custom domain icon' />
                             <h3>Custom Domain</h3>
                             <span>With Photosnap subscriptions you can host your stories on your own domain. You can 
@@ -64,7 +69,7 @@ class Features extends React.Component {
                             </span>
                         </div>
 
-                        <div>
+                        <div className='features-feature-card features-content-large-card'>
                             <img src={BoostExposureIMG} alt='Boost your exposure icon' />
                             <h3>Boost Your Exposure</h3>
                             <span>Users that viewed your story or gallery can easily get notifed of new and featured 
@@ -72,7 +77,7 @@ class Features extends React.Component {
                             </span>
                         </div>
 
-                        <div>
+                        <div className='features-feature-card features-content-large-card'>
                             <img src={DragDropIMG} alt='Drag and drop icon' />
                             <h3>Drag &amp; Drop Image</h3>
                             <span>Easily drag and drop your image and get beautiful shots everytime. No over the top 
